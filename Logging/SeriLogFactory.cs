44b22d0e8a345d385de4f4819ad324691ef6d69d
Sebastian Drela <sebastian.drela@wp.pl>
2024-03-26 21:53:50 +0100

    add docker file

--- Logging/SeriLogFactory.cs
+++ Logging/SeriLogFactory.cs
@@ -5,8 +5,8 @@ namespace MediPortSOAPI.Logging
     internal class SeriloggerFactory
     {
         private static readonly string _logPath = @$"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\MediPortSOAPI\logs.txt";
-
         private static ILogger _logger;
+
         public static ILogger GetLogger()
         {
             if (_logger is not null)
